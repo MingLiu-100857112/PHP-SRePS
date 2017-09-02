ef695137f16bc1b9fa4f68746b60ae014f263293
MingLiu-100857112 <liumingvic@gmail.com>
2017-09-02 22:42:44 +1000

    change var

--- scripts/main.js
+++ scripts/main.js
@@ -7,17 +7,7 @@ firebase.initializeApp({
   messagingSenderId: '137723334567'
 });
 
-const database = firebase.database().ref();
 
-database.child('records').on('value', snapshot => {
-  const array = $.map(snapshot.val(), value => { return value; });
-  const table = $('table'); $('tbody').remove();
-  const tbody = $('<tbody/>').appendTo(table);
-  $.each(array, i => {
-    const tr = $('<tr/>').appendTo(tbody);
-    const item = $('<td/>').addClass('mdl-data-table__cell--non-numeric').text(array[i].item).appendTo(tr);
-    const quantity = $('<td/>').text(array[i].quantity).appendTo(tr);
-    const date = $('<td/>').text(array[i].date).appendTo(tr);
   });
 });
 
