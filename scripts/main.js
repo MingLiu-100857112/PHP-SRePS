cd19d43622998f924bc3be16f73c9ef3aef0babe
MingLiu-100857112 <liumingvic@gmail.com>
2017-09-03 21:45:53 +0800

    Change setting

--- scripts/main.js
+++ scripts/main.js
@@ -22,16 +22,4 @@ database.child('records').on('value', snapshot => {
   });
 });
 
-$(document).ready(() => {
-  $('form').on('submit', event => {
-    const item = $('#item');
-    const quantity = $('#quantity');
-    const date = new Date();
-    const record = { item: item.val(), quantity: quantity.val(), date: date };
-    const recordKey = database.child('records').push().key;
-    const updates = {}; updates['/records/' + recordKey] = record;
-    database.update(updates);
-    quantity.val('');
-    item.val('');
-  });
-});
+
